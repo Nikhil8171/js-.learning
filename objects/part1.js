cee2baf2bb031d410c2278775eb226897a2a1727
nikhil_kumar <nkman2002@gmail.com>
2024-01-04 23:20:33 +0530

    objects declaration

---
+++ objects/part1.js
@@ -0,0 +1,11 @@
+const jsUser = {
+    [mySym1] : "mykiyone",
+    name : "nikhil",
+    age : 18,
+    locations : "jaipur",
+    email: "nkman300" ,
+
+}
+
+console.log(jsUser.age);
+console.log(jsUser[mySym1]);
