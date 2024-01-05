db1de9a9449bbf37ef6ac0e2b291d24649ccd643
nikhil_kumar <nkman2002@gmail.com>
2024-01-05 20:04:46 +0530

    declarationofobject

--- objects/part1.js
+++ objects/part1.js
@@ -1,5 +1,5 @@
 const jsUser = {
-    [mySym1] : "mykiyone",
+    // [mySym1] : "mykiyone",
     name : "nikhil",
     age : 18,
     locations : "jaipur",
@@ -7,5 +7,34 @@ const jsUser = {
 
 }
 
-console.log(jsUser.age);
-console.log(jsUser[mySym1]);
+// console.log(jsUser.age);
+// console.log(jsUser[mySym1]);
+
+let car1 = {
+ name: "nikhil",
+ manufactoring: "hinda",
+ fuelcapicity: 40,
+ greetme: function(){
+    console.log("hello my name is nikhil");
+
+ }
+}
+
+// console.log(car1.greetme());
+
+
+// 2 methods
+
+const detals = new Object();
+
+detals.name = "nikhil";
+detals.class = "bca";
+detals.rollno = 38;
+detals.aboutme = function(){
+    console.log("hello my name is nikhil");
+}
+
+console.log(detals.aboutme());
+
+
+
