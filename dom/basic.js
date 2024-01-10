d4800846900467b29a9b74ef1e8a486fbb07ff9c
nikhil_kumar <nkman2002@gmail.com>
2024-01-10 17:50:42 +0530

    dom basic

--- dom/basic.js
+++ dom/basic.js
@@ -1,4 +1,30 @@
-// console.log(window);
+console.log(window);
 
 let heading = document.getElementById("nik");
 console.dir(heading);
+
+myh2.forEach((h) => {
+    h.style.color = "red"
+    h.style.padding = "10px"
+    h.style.backgroundcolor = "greeen"
+})
+undefined
+myh2.forEach((h) => {
+    h.style.color = "red"
+    h.style.padding = "10px"
+    h.style.backgroundcolor = "green"
+})
+undefined
+myh2.forEach((h) => {
+    h.style.color = "red";
+    h.style.padding = "10px";
+    h.style.backgroundcolor = "green";
+})
+undefined
+myh2.forEach((h) => {
+    h.style.color = "red";
+    h.style.padding = "10px";
+    h.style.backgroundcolor = 'green';
+})
+
+
