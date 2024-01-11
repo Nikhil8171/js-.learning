3b85e0d40172e294f855bd9eea338abcf6cc36be
nikhil_kumar <nkman2002@gmail.com>
2024-01-12 00:42:40 +0530

    text acces

---
+++ innertextaccesinDOM/app2.js
@@ -0,0 +1,20 @@
+// innerText
+// textContent
+// innerHTML
+
+
+
+//class list 
+
+const h = document.querySelector('h1');
+console.log(h.classList);
+
+
+// add or remove class using js
+
+const p = document.querySelector('p');
+p.classList.add("paragraph");
+// p.classList.remove("paragraph")
+
+// change class name
+p.classList.toggle("para");
