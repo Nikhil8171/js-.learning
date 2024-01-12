e0419908eba3445058281184c00460295d19a9b9
nikhil_kumar <nkman2002@gmail.com>
2024-01-12 14:38:19 +0530

    otherbasis

---
+++ functions/codefunction.js
@@ -0,0 +1,21 @@
+function sayName (){
+    console.log("hello");
+}
+// sayName()
+
+
+function addtwonums (num1,num2){
+return num1+num2;
+}
+ //ole.log(addtwonums(3,4));
+
+// console.log(addtwonums("nik","hil"));
+
+
+let arr = [300,477,334,543]
+
+function getarr(geta){
+    return  geta[1];
+}
+
+console.log(getarr(arr));
